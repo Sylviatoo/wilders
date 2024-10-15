6589d171f16e833d1bf07460e878f6522413d3f0
sylviabarraud2@gmail.com <sylviabarraud2@gmail.com>
2024-10-15 12:25:18 +0200

    modif ex

--- wilders.js
+++ wilders.js
@@ -1,2 +1,9 @@
-import chalk from 'chalk';
-console.log(chalk.blue('Hello Tom, Rémi, Abdou, Charlotte!'));
+import chalk from 'chalk'
+
+const names = ["Tomeuh", "Rémieuh", "Abdoueuh", "Charlotteuh"];
+const colors = [chalk.blue, chalk.green, chalk.yellow, chalk.grey];
+
+names.forEach((name, index) =>
+    console.log (colors[index](name))
+);
+
